3df4fbd8a8264fed8030935e7bc49e34b19ec7b2
ivan7ce <frostow12h@gmail.com>
2020-12-13 18:52:58 +0300

    Add telemetry filtering by timestamp

--- src/main/kotlin/edu/escw/server/repository/Repositories.kt
+++ src/main/kotlin/edu/escw/server/repository/Repositories.kt
@@ -3,10 +3,12 @@ package edu.escw.server.repository
 import edu.escw.server.model.Device
 import edu.escw.server.model.Telemetry
 import edu.escw.server.model.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
-import org.springframework.stereotype.Repository
+import java.sql.Timestamp
 import java.util.*
 
 @RepositoryRestResource
@@ -21,4 +23,8 @@ interface UserRepository : JpaRepository<User, Long> {
 }
 
 @RepositoryRestResource
-interface TelemetryRepository : JpaRepository<Telemetry, Long> {}
+interface TelemetryRepository : JpaRepository<Telemetry, Long> {
+    @Query("SELECT t FROM Telemetry t WHERE t.tmstamp > TO_TIMESTAMP(?1, 'YYYY-MM-DD HH24:MI:SS')")
+    fun getAfter(timestamp: String, p: Pageable): Page<Telemetry>;
+}
+
